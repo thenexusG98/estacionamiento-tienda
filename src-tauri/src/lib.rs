c0e93d745dcd3d73e8ab9846f737e1ecc6df9da7
Uriel Gonzalez <uriel.gonzalez+ris@banregio.com>
2026-05-26 17:23:38 -0600

    refactor: remove redundant mobile entry point implementation from run function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -118,17 +118,3 @@ pub fn run() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
-     tauri::Builder::<tauri::Wry>::default()
-        .plugin(tauri_plugin_opener::init::<tauri::Wry>())
-        .plugin(tauri_plugin_sql::Builder::default().build::<tauri::Wry>())
-        .plugin(tauri_plugin_shell::init::<tauri::Wry>())
-        .plugin(tauri_plugin_fs::init::<tauri::Wry>())
-        .plugin(tauri_plugin_dialog::init::<tauri::Wry>())
-        .invoke_handler(tauri::generate_handler![print_raw_usb])
-        // Si usas updater:
-        // .plugin(tauri_plugin_updater::init::<tauri::Wry>())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
